5aae23c1edb3e96e7a1b70218c327f8761542fb2
guy michaely <michaelyguy@gmail.com>
2020-07-03 17:13:08 +0200

    working on part 1

--- src/register.js
+++ src/register.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-// import Greetee from "./welcome";
-
 export default class Register extends React.Component {
     constructor(props) {
         super(props);
@@ -9,61 +7,45 @@ export default class Register extends React.Component {
             name: "Kitty",
         };
     }
+    handleChange(e) {
+        this.setState({
+            first: e.target.value,
+            last: e.target.value,
+            email: e.target.value,
+            password: e.target.value,
+        });
+    }
     render() {
         return (
             <div className="register">
-                <input
-                    name="first"
-                    placeholder="first"
-                    onChange={(e) => this.handleChange(e)}
-                />
-                <input
-                    name="last"
-                    placeholder="last"
-                    onChange={(e) => this.handleChange(e)}
-                />
-                <input
-                    name="email"
-                    placeholder="email"
-                    onChange={(e) => this.handleChange(e)}
-                />
-                <input
-                    name="password"
-                    placeholder="password"
-                    onChange={(e) => this.handleChange(e)}
-                />
-                <button type="submit">Submit</button>
+                <form method="POST">
+                    <input
+                        name="first"
+                        type="text"
+                        placeholder="first"
+                        onChange={(e) => this.handleChange(e)}
+                    />
+                    <input
+                        name="last"
+                        type="text"
+                        placeholder="last"
+                        onChange={(e) => this.handleChange(e)}
+                    />
+                    <input
+                        name="email"
+                        type="text"
+                        placeholder="email"
+                        onChange={(e) => this.handleChange(e)}
+                    />
+                    <input
+                        name="password"
+                        type="password"
+                        placeholder="password"
+                        onChange={(e) => this.handleChange(e)}
+                    />
+                    <button type="submit">Submit</button>
+                </form>
             </div>
         );
     }
 }
-
-// export default class Hello extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             name: "Kitty",
-//         };
-//     }
-//     handleChange(e) {
-//         this.setState({
-//             name: e.target.value,
-//         });
-//     }
-//     render() {
-//         return (
-//             <div
-//                 className="awesome"
-//                 style={{
-//                     color: "tomato",
-//                     textDecoration: "underline",
-//                 }}
-//             >
-//                 Hello, <Greetee name={this.state.name} />!
-//                 <p>Today is {this.props.day}</p>
-//                 <input name="name" onChange={(e) => this.handleChange(e)} />
-//                 <small>{this.state.name}</small>
-//             </div>
-//         );
-//     }
-// }
--- src/welcome.js
+++ src/welcome.js
@@ -1,5 +1,12 @@
 import React from "react";
+import Register from "./register";
 
 export default function Welcome() {
-    return <div>You are rly welcome</div>;
+    return (
+        <div>
+            <img src="logo.png" />
+            <h3>You are rly welcome</h3>
+            <Register />
+        </div>
+    );
 }
