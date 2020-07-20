c41489f91eb2f1c470b750fa5c5033f9058ccda0
guy michaely <michaelyguy@gmail.com>
2020-07-21 00:23:24 +0200

    :(

--- src/about.js
+++ src/about.js
@@ -1,9 +1,12 @@
 import React, { useState, useRef } from "react";
 import { useSpring, animated } from "react-spring";
 
-export default function About({ elemRef }) {
-    const closeRef = useRef();
+export default function About(props) {
+    console.log("props", props);
 
+    const hideShow = useSpring({
+        opacity: props.isToggled ? 1 : 0,
+    });
     // const handleClick = () => {
     //     closeRef.current.style.visibility = "hidden";
     // };
@@ -12,9 +15,8 @@ export default function About({ elemRef }) {
     //     closeRef.current.style.visibility = "hidden";
     // };
 
-    ////////// CLOSE ////////////
     return (
-        <div ref={elemRef} className="about-container">
+        <animated.div style={hideShow} className="about-container">
             <span className="close-about">close</span>
             <h2>Hi, I’m Guy, Berlin-based graphic designer.</h2>
             <br />
@@ -31,6 +33,43 @@ export default function About({ elemRef }) {
                 conceptual solutions for editorials, visual identities for other
                 printed matter and web design.
             </p>
-        </div>
+        </animated.div>
     );
 }
+
+// export default function About({ elemRef }) {
+//     const closeRef = useRef();
+
+//     // const handleClick = () => {
+//     //     closeRef.current.style.visibility = "hidden";
+//     // };
+
+//     // const hover = () => {
+//     //     closeRef.current.style.visibility = "hidden";
+//     // };
+
+//     return (
+//         <animated.div
+//             style={elemRef.btn}
+//             ref={elemRef}
+//             className="about-container"
+//         >
+//             <span className="close-about">close</span>
+//             <h2>Hi, I’m Guy, Berlin-based graphic designer.</h2>
+//             <br />
+//             <p>
+//                 I grew up in Tel-Aviv and studied graphic design at the
+//                 Department of Visual Communication at Shenkar College of
+//                 Engineering and Design. Through my studies, I have gained a lot
+//                 of knowledge and experience in different fields, and ultimately,
+//                 chose to focus on UX UI and web design. As I see it, a good
+//                 graphic design is the result of deep exploration and
+//                 contemplation and is usually backed by a strong concept. My
+//                 ambition is to work in close collaboration with enthusiastic
+//                 people, artists, publishers, and institutions. I provide
+//                 conceptual solutions for editorials, visual identities for other
+//                 printed matter and web design.
+//             </p>
+//         </animated.div>
+//     );
+// }
--- src/welcome.js
+++ src/welcome.js
@@ -5,25 +5,21 @@ import { useSpring, animated } from "react-spring";
 
 export default function Welcome() {
     const [isToggled, setToggle] = useState(false);
-    const aboutRef = useRef();
-
-    const handleClick = () => {
-        aboutRef.current.style.visibility = "visible";
-    };
 
     const fade = useSpring({
         opacity: 1,
         from: { opacity: 0 },
     });
-    const hideShow = useSpring({
-        opacity: isToggled ? 1 : 0,
-    });
+
+    // const hideShow = useSpring({
+    //     opacity: isToggled ? 1 : 0,
+    // });
 
     return (
         <div>
             <animated.header style={fade}>
                 <h1>Guy Michaely</h1>
-                <h1 onClick={handleClick} id="about">
+                <h1 onClick={() => setToggle(!isToggled)} id="about">
                     About
                 </h1>
             </animated.header>
@@ -31,8 +27,42 @@ export default function Welcome() {
             <div className="flier">
                 <img src="smile.png" />
             </div>
-            {/* <About /> */}
-            <About style={fade} elemRef={aboutRef} />
+            <About toggle={isToggled} />
         </div>
     );
 }
+
+// export default function Welcome() {
+//     const [isToggled, setToggle] = useState(false);
+//     const aboutRef = useRef();
+
+//     const handleClick = () => {
+//         aboutRef.current.style.visibility = "visible";
+//     };
+
+//     const fade = useSpring({
+//         opacity: 1,
+//         from: { opacity: 0 },
+//     });
+
+//     const hideShow = useSpring({
+//         opacity: isToggled ? 1 : 0,
+//     });
+
+//     return (
+//         <div>
+//             <animated.header style={fade}>
+//                 <h1>Guy Michaely</h1>
+//                 <h1 onClick={handleClick} id="about">
+//                     About
+//                 </h1>
+//             </animated.header>
+//             {/* <Toggle /> */}
+//             <div className="flier">
+//                 <img src="smile.png" />
+//             </div>
+//             {/* <About /> */}
+//             <About style={fade} elemRef={aboutRef} />
+//         </div>
+//     );
+// }
