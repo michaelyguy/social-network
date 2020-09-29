995ab366ef70f2f41bb9b5047dedf2d9470fa2cb
guy michaely <michaelyguy@gmail.com>
2020-09-29 13:52:37 +0200

    chat redesign

--- src/chat.js
+++ src/chat.js
@@ -1,27 +1,21 @@
 import React, { useEffect, useRef } from "react";
 import { socket } from "./socket";
 import { useSelector } from "react-redux";
+import Draggable from "react-draggable";
 
 export default function Chat() {
     const elemRef = useRef();
     const chatMessages = useSelector((state) => {
-        console.log("here are my last 10 msgs", state.chatmsgs);
         return state && state.chatmsgs;
     });
 
     //// we want this to run everytime you get a new chat msg ////
     useEffect(() => {
-        // console.log("elemnt Ref: ", elemRef);
-        // console.log("scroll top: ", elemRef.current.scrollTop);
-        // console.log("client height: ", elemRef.current.clientHeight);
-        // console.log("scroll height: ", elemRef.current.scrollHeight);
         elemRef.current.scrollTop =
             elemRef.current.scrollHeight - elemRef.current.clientHeight;
     }, []);
 
     const keyCheck = (e) => {
-        // console.log("value: ", e.target.value);
-        // console.log("key pressed: ", e.key);
         if (e.key === "Enter") {
             e.preventDefault();
             console.log("our msg: ", e.target.value);
@@ -29,28 +23,44 @@ export default function Chat() {
             e.target.value = "";
         }
     };
-    // console.log("----chatMessages---");
-    // console.log(chatMessages);
+
     return (
-        <div className="chat-big">
-            <h2 className="chat-title">Chat</h2>
-            <div className="chat-messages-container" ref={elemRef}>
-                {chatMessages &&
-                    chatMessages.map((msg) => (
-                        <div className="each-msg" key={msg.id}>
-                            <img className="chat-userpic" src={msg.imgurl} />
-                            <p>
-                                {msg.first} {msg.last}
-                            </p>
-                            <p>{msg.message}</p>
-                        </div>
-                    ))}
+        <Draggable>
+            <div className="card-wrapper-chat">
+                <div id="card-header" className="card-header">
+                    <div className="card-close">
+                        <div className="minimize"></div>
+                    </div>{" "}
+                    <div className="card-title">Chat</div>
+                    <div className="card-move">
+                        <img className="move-png" src="move.png" />
+                    </div>
+                </div>
+                <div className="card-content">
+                    <div className="chat-messages-container" ref={elemRef}>
+                        {chatMessages &&
+                            chatMessages.map((msg) => (
+                                <div className="each-msg" key={msg.id}>
+                                    <img
+                                        className="chat-userpic"
+                                        src={msg.imgurl}
+                                    />
+                                    <p className="user-line">User</p>
+                                    <p className="info">
+                                        {msg.first} {msg.last}
+                                    </p>
+                                    <p className="user-line">Comment</p>
+                                    <p className="info">{msg.message}</p>
+                                </div>
+                            ))}
+                    </div>
+                    <textarea
+                        className="chat-area"
+                        placeholder="Add comment.."
+                        onKeyDown={keyCheck}
+                    ></textarea>
+                </div>
             </div>
-            <textarea
-                className="chat-area"
-                placeholder="add text here"
-                onKeyDown={keyCheck}
-            ></textarea>
-        </div>
+        </Draggable>
     );
 }
