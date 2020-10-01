6b052a8a5258d9c5c88948ca51bb278310dd9fce
guy michaely <michaelyguy@gmail.com>
2020-10-01 12:43:20 +0200

    video

--- src/Petition.js
+++ src/Petition.js
@@ -77,12 +77,14 @@ export default function Petition() {
                                 controls="true"
                                 className="gif-project"
                             >
-                                <source src="petition.mp4" type="video/mp4" />
+                                <source src="petition1.mp4" type="video/mp4" />
                             </video>
                             {/* <video
                                 ref={vidRef}
                                 muted
                                 loop
+                                playsinline
+                                controls="true"
                                 className="gif-project"
                             >
                                 <source src="petition.mp4" type="video/mp4" />
