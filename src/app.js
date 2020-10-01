8389ae1777c632d9b3b05585318a51760e34de09
guy michaely <michaelyguy@gmail.com>
2020-10-01 15:31:21 +0200

    vid

--- src/app.js
+++ src/app.js
@@ -142,7 +142,14 @@ export default function App() {
                     <Petition />
                     <ImageBoard />
                     <SocialNetwork />
-                    <video autoPlay muted loop className="gif-project">
+                    <video
+                        width="320"
+                        height="240"
+                        autoPlay
+                        muted
+                        loop
+                        className="gif-project"
+                    >
                         <source src="petition.mp4" type="video/mp4"></source>
                     </video>
                 </div>
